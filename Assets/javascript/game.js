6930b929d626ed2dd91e7c6a9cc0b1eb37b4a833
Tyler Larrabee <tyer.k.larrabee@gmail.com>
2019-04-14 20:45:47 -0700

    css fix

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -6,6 +6,15 @@ $(document).ready(function () {
     var highScore = 0;
     // var answer;
     var time = levelUp();
+    var intervalId;
+    var userSelection;
+
+    // Starts the game
+    function startGame() {
+        intervalId = setInterval(countDown, 1000);
+        mathProb();
+    }
+    // sets the timer and shortens the interval as the user adds more 
     function levelUp() {
         if (correctAnswers < 5) {
             return 8;
@@ -21,15 +30,7 @@ $(document).ready(function () {
         }
     }
 
-    var intervalId;
-    var userSelection;
-
-    // Starts the game
-    function startGame() {
-        intervalId = setInterval(countDown, 1000);
-        mathProb();
-    }
-
+    // stops game when timer expires or user selects/enters incorrect answer. 
     function stopGame() {
         clearInterval(intervalId);
         alert("You lost. Score: " + correctAnswers);
@@ -41,6 +42,7 @@ $(document).ready(function () {
         correctAnswers = 0;
     }
 
+    // resets the timer on correct answer invoked by mathProb class
     function reset() {
         clearInterval(intervalId);
         $("#maths").empty();
@@ -49,21 +51,21 @@ $(document).ready(function () {
         intervalId = setInterval(countDown, 1000);
     }
 
+    // countDown timer, stops game when timer reaches 0.
     function countDown() {
         time--;
-        // $("#countdown").text(time);
         $("#countdown").html("<h1 class='display-4'>"+time+"</h1>");
         if (time === 0) {
             stopGame();
             return;
         }
     }
-
+// generates 2 random numbers 
     function mathProb() {
         $("#maths").empty();
         var numOne = Math.floor(Math.random() * 65) + 3;
         var numTwo = Math.floor(Math.random() * 67) + 3;
-        // + / - *
+
         var randomOperator = Math.floor(Math.random() * 2);
         var answer = mathtastic(randomOperator, numOne, numTwo);
         inputDisplay(answer, numOne, numTwo, randomOperator);
@@ -84,14 +86,13 @@ $(document).ready(function () {
             }
         });
     };
-
+// A proper name for this would be answer or something.
+// This calculates the solution. Subraction equations will not have negative solution values.
     function mathtastic(op, nOne, nTwo) {
         var mathyMath;
         if (op === 0) {
             if (nOne >= nTwo) {
                 mathyMath = nOne - nTwo;
-                console.log("Answer: " + mathyMath);
-                console.log("nOne: " + nOne + " nTwo: " + nTwo);
                 return mathyMath;
             }
             else {
@@ -100,11 +101,12 @@ $(document).ready(function () {
             }
         } else if (op === 1) {
             mathyMath = nOne + nTwo;
-            // console.log(mathyMath);
             return mathyMath;
         }
     }
-
+// this displays the math problem and the inputs. 
+// If you are smart enough, the quiz will change from multiple choice to number entry.
+// 
     function inputDisplay(sol, nOne, nTwo, opp) {
         $("#input").empty();
         $("#maths").empty();
@@ -124,10 +126,13 @@ $(document).ready(function () {
             operator = " + ";
         }
         $("#maths").append(firstNum + operator + secondNum);
+// this randomizes the location of the correct answer. 
         if (correctAnswers < 10) {
+// selects a number between 1 and 5 and adds it to the answer.
+// loop will always reach the correct number
             var range = Math.floor(Math.random() * 5) + 1 + sol, nOne, nTwo;
-            // console.log("RANGE " + range)
             for (let i = 5; i >= 0; i--) {
+// if the solution is < 5 this prevents negative answers from showing up because the solution will be >= 0
                 if (range === 0) {
                     range += 10;
                 } else {
@@ -149,6 +154,7 @@ $(document).ready(function () {
             form.focus();
         }
     }
+// DO MATH!!!!
     $("#start").click(function(){
         $("#startScreen").hide();
         $("#mathArea").attr("hidden", false);
